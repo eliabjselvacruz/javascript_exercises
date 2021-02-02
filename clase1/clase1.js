c64860e0d4001af4eb88e4d032dd430188c63a86
eliabjselvacruz <eliabjselvacruz@gmail.com>
2021-02-02 15:14:00 -0600

    Ejercicio de la clase 1.

---
+++ clase1/clase1.js
@@ -0,0 +1,7 @@
+console.log("Hola mundo");
+var nombre, apellido;
+nombre = "Eliab";
+apellido = "Selva"; 
+edad = 22;
+console.log("Hola "+nombre+" "+apellido);
+console.log("Tengo la edad de: "+edad+" años");
